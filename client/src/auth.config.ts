ebb2913589dc870e10243f45c5ebb7c2348f1121
Rohit luni <rohitluni4242@gmail.com>
2025-02-28 04:08:59 +0530

    init

--- client/src/auth.config.ts
+++ client/src/auth.config.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcryptjs";
+import { compare } from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "./lib/schemas";
@@ -26,7 +26,7 @@ export default {
 
           if (!user || !user.password) return null;
 
-          const passwordsMatch = await bcrypt.compare(password, user.password);
+          const passwordsMatch = await compare(password, user.password);
 
           if (passwordsMatch) return user;
         }
